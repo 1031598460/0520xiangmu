8e706dbbdd89b13a38d260e21b74785f70a42e0d
yu_demo <1031598460@qq.com>
2019-09-09 20:21:00 +0800

    基本配置

---
+++ src/redux/reducers.js
@@ -0,0 +1,11 @@
+import { combineReducers} from 'redux'
+
+function f(prevState={},action) {
+    switch (action.type) {
+        default:
+            return prevState;
+    }
+}
+export default combineReducers({
+    f
+})
---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux'
+import  thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import  reducers from './reducers';
+
+let store;
+if(process.env.NODE_ENV === 'development'){
+    store=createStore(reducers,composeWithDevTools(applyMiddleware(thunk)))
+}else{
+    store=createStore(reducers,applyMiddleware(thunk))
+}
+export default store;
